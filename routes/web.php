8e0a8e376a866cc587f634ba5991d14ebf46a86d
alex-anie <ocxigin@gmail.com>
2025-08-06 15:35:29 +0100

    Day 19 -: Routes Reloaded

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ Route::get('/', function () {
     return view('home');
 });
 
+// Index :: get all jobs
 Route::get('/jobs', function ()  {
     $jobs = Job::with('employer')->latest()->simplePaginate(3);
 
@@ -15,15 +16,18 @@ Route::get('/jobs', function ()  {
     ]);
 });
 
+// Create :: display a form to create a job
 Route::get('/jobs/create', function(){
     return view('jobs.create');
 });
 
+// Show :: display a page of a specific job selected
 Route::get('/jobs/{id}', function ($id)  {
     $job = Job::find($id);
     return view('jobs.show', ['job' => $job]);
 });
 
+// Store :: this story a new job entery into the Database.
 Route::post('/jobs', function(){
     request()->validate([
         'title' => ['required', 'min:3'],
@@ -35,6 +39,45 @@ Route::post('/jobs', function(){
         'employer_id' => 1
     ]);
 
+    return redirect('/jobs');
+});
+
+// Edit :: display a page of a specific job for editing
+Route::get('/jobs/{id}/edit', function ($id)  {
+    $job = Job::find($id);
+    return view('jobs.edit', ['job' => $job]);
+});
+
+// Update :: create the action to update the page
+Route::patch('/jobs/{id}', function ($id)  {
+   // validate
+    request()->validate([
+        'title' => ['required', 'min:3'],
+        'salary' => ['required']
+    ]);
+
+   // authorize (on hold ...)
+
+   // update the job
+   $job = Job::findOrFail($id);
+
+   $job->update([
+        'title' => request('title'),
+        'salary' => request('salary')
+   ]);
+
+   // redirect to the job patch
+    return redirect('/jobs/' . $job->id);
+});
+
+// Destroy :: Delete a record from the database
+Route::delete('/jobs/{id}', function ($id)  {
+    // authorize the hold ...
+
+    // delete the job
+    Job::findOrFail($id)->delete();
+
+    // redirect
     return redirect('/jobs');
 });
 
