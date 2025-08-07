506cf73d80454fb91b2f5d257ec0e2a894e77e9f
alex-anie <ocxigin@gmail.com>
2025-08-07 12:26:21 +0100

    Day 22 -: Make a Login and Registration from scratch: Part 2

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,20 @@
 <?php
 
 use App\Http\Controllers\JobController;
+use App\Http\Controllers\RegisteredUserController;
+use App\Http\Controllers\SessionController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'home');
 Route::resource('jobs', JobController::class);
 Route::view('/contact', 'contact');
+
+// Auth
+Route::get('/register', [RegisteredUserController::class, 'create']);
+Route::post('/register', [RegisteredUserController::class, 'store']);
+
+Route::get('/login', [SessionController::class, 'create']);
+Route::post('/login', [SessionController::class, 'store']);
 
 // Route::controller(JobController::class)->group(function(){
 //     Route::get('/jobs',  'index');
@@ -16,7 +25,6 @@ Route::view('/contact', 'contact');
 //     Route::patch('/jobs/{job}',  'update');
 //     Route::delete('/jobs/{job}',  'destroy');
 // });
-
 
 /*
     Commented
